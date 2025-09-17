1aefcf7e9ff88b345ba87551746445d1bce16ed4
hyemin33 <joliie@naver.com>
2025-09-17 21:03:41 +0900

    feat: 2.8) 페이지별 레이아웃 설정하기

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { ReactNode } from "react";
 import style from "./index.module.css";
+import SearchableLayout from "@/components/searchable-layout";
 
 export default function Home() {
   return <h1 className={style.h1}>index</h1>;
 }
+
+Home.getLaout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
