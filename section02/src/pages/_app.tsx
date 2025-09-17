2de7d9a71a778f38ca2cb0706aa143b3227f62e0
hyemin33 <joliie@naver.com>
2025-09-17 19:40:56 +0900

    feat: 2.4) 프리페칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -9,12 +10,20 @@ export default function App({ Component, pageProps }: AppProps) {
   const onClickButton = () => {
     router.push("/book");
   };
-  
+
+  // 마운트될때 프리패칭을 해주고 싶다면 router.prefetch를 사용한다.
+  useEffect(() => {
+    router.prefetch("/book");
+  }, []);
+
   return (
     <>
       <header>
         <Link href={"/"}>index</Link> &nbsp;
-        <Link href={"/search"}>search</Link> &nbsp;
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        &nbsp;
         <Link href={"/book"}>book</Link> &nbsp;
         <div>
           <button type="button" onClick={onClickButton}>
